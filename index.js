132243fdeb54b836c5ace0c29ef25c67ea4f5593
tranad2 <atran116@gmail.com>
2018-10-07 21:34:02 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Index.ios.js - place code in here for IOS
+
+// Import a library to help create a component
+import React from 'react';
+import { View, AppRegistry } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+// Create a component
+const App = () => (
+    <View style={{ flex: 1}}>
+        <Header headerText={'Albums'}/>
+        <AlbumList />
+    </View>
+);
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
